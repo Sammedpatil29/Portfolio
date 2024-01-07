f50e167a9beb0a3123f700bc128e6618c6d92758
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2024-01-07 18:53:31 +0530

    changes in skills

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
   cards = [
-    { title: 'REVA University', year: '2016 - 2020', branch: 'B.Tech in EEE', result: '7.64 CGPA',image: '/assets/map.png' },
-    { title: 'ACS Dharwad', year: '2014 - 2016', branch: 'Pre University', result: '85.17%',image: '/assets/map.png' },
-    { title: 'AVVS Athani', year: '2013 - 2014', branch: '10th Board', result: '84.96%',image: '/assets/map.png' }
+    { title: 'REVA University', year: '2016 - 2020', branch: 'B.Tech in EEE', result: '7.64 CGPA',image: '/assets/reva.png' },
+    { title: 'ACS Dharwad', year: '2014 - 2016', branch: 'Pre University', result: '85.17%',image: '/assets/acs.png' },
+    { title: 'AVVS Athani', year: '2013 - 2014', branch: '10th Board', result: '84.96%',image: '/assets/avvs.png' }
   ];
   constructor() { }
 
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
 
   logos = [
-    {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
-    {skill: 'assets/4672509.png'},
-    {skill: 'assets/5848152fcef1014c0b5e4967.png'},
-    {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+    {difficulty:'good', skill: 'assets/5847ea22cef1014c0b5e4833.png'},
+    {difficulty:'average', skill: 'assets/4672509.png'},
+    {difficulty:'good', skill: 'assets/5848152fcef1014c0b5e4967.png'},
+    {difficulty:'green', skill: 'assets/google-cloud_logo-freelogovectors 1.png'},
+    {difficulty:'good', skill: 'assets/5847ea22cef1014c0b5e4833.png'},
+    {difficulty:'beginner', skill: 'assets/4672509.png'},
+    {difficulty:'good', skill: 'assets/5848152fcef1014c0b5e4967.png'},
+    {difficulty:'average', skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
   ]
   logos2 = [
     {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
@@ -19,6 +23,7 @@ export class SkillsComponent implements OnInit {
     {skill: 'assets/5848152fcef1014c0b5e4967.png'},
     {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
   ]
+
   constructor() { }
 
   ngOnInit(): void {
