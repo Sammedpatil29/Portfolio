ea3c3006d898f0db2e95ace09f5df6129a470bb7
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2024-01-07 14:54:37 +0530

    added skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  logos = [
+    {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
+    {skill: 'assets/4672509.png'},
+    {skill: 'assets/5848152fcef1014c0b5e4967.png'},
+    {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+  ]
+  logos2 = [
+    {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
+    {skill: 'assets/4672509.png'},
+    {skill: 'assets/5848152fcef1014c0b5e4967.png'},
+    {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
