41b6d62dad2d7fa164fec0543e0db54393206a70
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2024-01-13 15:46:41 +0530

    github page changes

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -18,10 +18,10 @@ export class SkillsComponent implements OnInit {
     {difficulty:'average', skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
   ]
   logos2 = [
-    {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
-    {skill: 'assets/4672509.png'},
-    {skill: 'assets/5848152fcef1014c0b5e4967.png'},
-    {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+    {skill: 'assets/canva-logo-0.png'},
+    {skill: 'assets/download (1).png'},
+    {skill: 'assets/png-transparent-adobe-premiere-pro-cc-14-3-hd-logo-thumbnail.png'},
+    {skill: 'assets/iBJehkOi_400x400.png'}
   ]
 
   constructor() { }
