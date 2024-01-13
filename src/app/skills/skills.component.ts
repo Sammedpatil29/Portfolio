244bbf3f1353e06d5f7db38a35aab295c09c2b92
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2024-01-13 19:53:41 +0530

    Added resume download button. made changes in skills and projects.

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -12,16 +12,15 @@ export class SkillsComponent implements OnInit {
     {difficulty:'average', skill: 'assets/4672509.png'},
     {difficulty:'good', skill: 'assets/5848152fcef1014c0b5e4967.png'},
     {difficulty:'green', skill: 'assets/google-cloud_logo-freelogovectors 1.png'},
-    {difficulty:'good', skill: 'assets/5847ea22cef1014c0b5e4833.png'},
-    {difficulty:'beginner', skill: 'assets/4672509.png'},
-    {difficulty:'good', skill: 'assets/5848152fcef1014c0b5e4967.png'},
-    {difficulty:'average', skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+    {difficulty:'good', skill: 'assets/assets/github.png'},
   ]
   logos2 = [
-    {skill: 'assets/5847ea22cef1014c0b5e4833.png'},
-    {skill: 'assets/4672509.png'},
-    {skill: 'assets/5848152fcef1014c0b5e4967.png'},
-    {skill: 'assets/google-cloud_logo-freelogovectors 1.png'}
+    {skill: 'assets/after effect.png'},
+    {skill: 'assets/canva.png'},
+    {skill: 'assets/capcut.png'},
+    {skill: 'assets/chatgpt.png'},
+    {skill: 'assets/Google-Bard.png'},
+    {skill: 'assets/premierpro.png'}
   ]
 
   constructor() { }
